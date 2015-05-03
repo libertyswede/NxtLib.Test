7f165c3e033809548a787f8705675dd0c9c1516d
Liberty Swede <libertyswede@outlook.com>
2015-05-03 14:59:13 +0200

    Test fixes in CastVoteTest

--- Src/NxtLib.Test/VotingSystem/CastVoteTest.cs
+++ Src/NxtLib.Test/VotingSystem/CastVoteTest.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using NLog;
 using NxtLib.VotingSystem;
 
 namespace NxtLib.Test.VotingSystem
@@ -24,18 +23,9 @@ namespace NxtLib.Test.VotingSystem
                 CreateTransaction.CreateTransactionBySecretPhrase(true)).Result;
 
             var attachment = (MessagingVoteCastingAttachment)castVoteReply.Transaction.Attachment;
-            if (attachment.Votes.Count != 2)
-            {
-                Logger.Error("Unexpected vote count, expected: 2, actual: {0}", attachment.Votes.Count);
-            }
-            if (attachment.Votes[0] != 1)
-            {
-                Logger.Error("Unexpected vote value at index 0, expected: 1, actual: {0}", attachment.Votes[0]);
-            }
-            if (attachment.Votes[1] != -128)
-            {
-                Logger.Error("Unexpected vote value at index 1, expected: -128, actual: {0}", attachment.Votes[0]);
-            }
+            Compare(2, attachment.Votes.Count, "vote count");
+            Compare(1, attachment.Votes[0], "vote value at index 0");
+            Compare(-128, attachment.Votes[1], "vote value at index 1");
         }
     }
 }
