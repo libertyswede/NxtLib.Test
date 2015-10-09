35a72780a27563c076c955916fb9e854975d949f
Liberty Swede <libertyswede@outlook.com>
2015-10-09 16:00:20 +0200

    Added tests

--- Src/NxtLib.Test/Transactions/TransactionServiceTest.cs
+++ Src/NxtLib.Test/Transactions/TransactionServiceTest.cs
@@ -13,6 +13,44 @@ namespace NxtLib.Test.Transactions
 
         internal void RunAllTests()
         {
+            TestCalculateFullHash();
+            TestGetExpectedTransactions();
+            TestSignTransaction();
+        }
+
+        private void TestCalculateFullHash()
+        {
+            using (Logger = new TestsessionLogger())
+            {
+                var accountService = TestSettings.ServiceFactory.CreateAccountService();
+                var unsignedSendMoney = accountService.SendMoney(CreateTransaction.CreateTransactionByPublicKey(), TestSettings.Account2Rs, Amount.OneNqt).Result;
+                var signedTransaction = _service.SignTransaction(new TransactionParameter(unsignedSendMoney.UnsignedTransactionBytes), TestSettings.SecretPhrase).Result;
+                var signatureHash = signedTransaction.Transaction.SignatureHash;
+
+                var calculateFullHash = _service.CalculateFullHash(new BinaryHexString(signatureHash), unsignedSendMoney.UnsignedTransactionBytes).Result;
+                AssertEquals(signedTransaction.FullHash.ToHexString(), calculateFullHash.FullHash.ToHexString(), "FullHash");
+            }
+        }
+
+        private void TestSignTransaction()
+        {
+            using (Logger = new TestsessionLogger())
+            {
+                var amount = Amount.OneNqt;
+                var accountService = TestSettings.ServiceFactory.CreateAccountService();
+                var recipient = TestSettings.Account2Rs;
+                var unsignedSendMoney = accountService.SendMoney(CreateTransaction.CreateTransactionByPublicKey(), recipient, amount).Result;
+
+                var signedTransaction = _service.SignTransaction(new TransactionParameter(unsignedSendMoney.UnsignedTransactionBytes), TestSettings.SecretPhrase).Result;
+
+                var transaction = signedTransaction.Transaction;
+                AssertEquals(transaction.Amount.Nqt, Amount.OneNqt.Nqt, "Amount");
+                AssertEquals(transaction.RecipientRs, recipient, "RecipientRs");
+            }
+        }
+
+        private void TestGetExpectedTransactions()
+        {
             using (Logger = new TestsessionLogger())
             {
                 var result = _service.GetExpectedTransactions().Result;
