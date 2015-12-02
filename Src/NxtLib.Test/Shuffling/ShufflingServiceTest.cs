d398f2c57562a860b8d92b1e3fe9a49b0e2e3f74
Liberty Swede <libertyswede@outlook.com>
2015-12-02 17:21:26 +0100

    Shufflingtests

--- Src/NxtLib.Test/Shuffling/ShufflingServiceTest.cs
+++ Src/NxtLib.Test/Shuffling/ShufflingServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.Extensions.Logging;
 using NxtLib.Shuffling;
 
@@ -12,6 +13,7 @@ namespace NxtLib.Test.Shuffling
     {
         private readonly ILogger _logger;
         private readonly IShufflingService _shufflingService;
+        private readonly ulong _knownShufflingId = 17626955119804441745;
 
         public ShufflingServiceTest(ILogger logger, IShufflingService shufflingService)
         {
@@ -23,14 +25,11 @@ namespace NxtLib.Test.Shuffling
         {
             TestGetAccountShufflings();
             TestGetAllShufflings();
-        }
-
-        private void TestGetAllShufflings()
-        {
-            using (Logger = new TestsessionLogger(_logger))
-            {
-                var shufflings = _shufflingService.GetAccountShufflings(TestSettings.Account1, true).Result;
-            }
+            TestGetAssignedShufflings();
+            TestGetHoldingShufflings();
+            //TestGetShufflers();
+            TestGetShuffling();
+            TestGetShufflingParticipants();
         }
 
         private void TestGetAccountShufflings()
@@ -39,23 +38,100 @@ namespace NxtLib.Test.Shuffling
             {
                 var shufflings = _shufflingService.GetAccountShufflings(TestSettings.Account1, true).Result;
 
-                var knownShuffling = shufflings.Shufflings.Single(s => s.ShufflingId == 17626955119804441745);
+                var knownShuffling = shufflings.Shufflings.Single(s => s.ShufflingId == _knownShufflingId);
 
                 AssertEquals(2, shufflings.Shufflings.Count(), "Shufflings.Count()");
-                AssertIsNull(knownShuffling.AssigneeId, nameof(knownShuffling.AssigneeId));
-                AssertIsNull(knownShuffling.AssigneeRs, nameof(knownShuffling.AssigneeRs));
-                AssertEquals(1234, knownShuffling.Amount.Nxt, nameof(knownShuffling.Amount));
-                AssertEquals(0, knownShuffling.BlocksRemaining, nameof(knownShuffling.BlocksRemaining));
-                AssertEquals(0, knownShuffling.HoldingId, nameof(knownShuffling.HoldingId));
-                AssertEquals(HoldingType.Nxt, knownShuffling.HoldingType, nameof(knownShuffling.HoldingType));
-                AssertEquals(5873880488492319831, knownShuffling.IssuerId, nameof(knownShuffling.IssuerId));
-                AssertEquals("NXT-XK4R-7VJU-6EQG-7R335", knownShuffling.IssuerRs, nameof(knownShuffling.IssuerRs));
-                AssertEquals(5, knownShuffling.ParticipantCount, nameof(knownShuffling.ParticipantCount));
-                AssertEquals(5, knownShuffling.RecipientPublicKeys.Count, nameof(knownShuffling.RecipientPublicKeys));
-                AssertEquals("9164b2334a869ff4db6795777d4afd6aaaff4217dc47cb1baa42f43530ee9e93", knownShuffling.ShufflingFullHash.ToHexString(), nameof(knownShuffling.ShufflingFullHash));
-                AssertEquals("cb8c1482560052b87ff992e567cfdc7bf13a1f069a1df3f81de98183baf8906e", knownShuffling.ShufflingStateHash.ToHexString(), nameof(knownShuffling.ShufflingStateHash));
-                AssertEquals(ShufflingStage.Done, knownShuffling.Stage, nameof(knownShuffling.Stage));
-            }   
+                AssertKnownShuffling(knownShuffling);
+            }
+        }
+
+        private static void AssertKnownShuffling(ShufflingData knownShuffling)
+        {
+            AssertIsNull(knownShuffling.AssigneeId, nameof(knownShuffling.AssigneeId));
+            AssertIsNull(knownShuffling.AssigneeRs, nameof(knownShuffling.AssigneeRs));
+            AssertEquals(1234, knownShuffling.Amount.Nxt, nameof(knownShuffling.Amount));
+            AssertEquals(0, knownShuffling.BlocksRemaining, nameof(knownShuffling.BlocksRemaining));
+            AssertEquals(0, knownShuffling.HoldingId, nameof(knownShuffling.HoldingId));
+            AssertEquals(HoldingType.Nxt, knownShuffling.HoldingType, nameof(knownShuffling.HoldingType));
+            AssertEquals(5873880488492319831, knownShuffling.IssuerId, nameof(knownShuffling.IssuerId));
+            AssertEquals("NXT-XK4R-7VJU-6EQG-7R335", knownShuffling.IssuerRs, nameof(knownShuffling.IssuerRs));
+            AssertEquals(5, knownShuffling.ParticipantCount, nameof(knownShuffling.ParticipantCount));
+            AssertEquals(5, knownShuffling.RecipientPublicKeys.Count, nameof(knownShuffling.RecipientPublicKeys));
+            AssertEquals("9164b2334a869ff4db6795777d4afd6aaaff4217dc47cb1baa42f43530ee9e93",
+                knownShuffling.ShufflingFullHash.ToHexString(), nameof(knownShuffling.ShufflingFullHash));
+            AssertEquals("cb8c1482560052b87ff992e567cfdc7bf13a1f069a1df3f81de98183baf8906e",
+                knownShuffling.ShufflingStateHash.ToHexString(), nameof(knownShuffling.ShufflingStateHash));
+            AssertEquals(ShufflingStage.Done, knownShuffling.Stage, nameof(knownShuffling.Stage));
+        }
+
+        private void TestGetAllShufflings()
+        {
+            using (Logger = new TestsessionLogger(_logger))
+            {
+                // Just testing for exceptions here
+                // ReSharper disable once UnusedVariable
+                var shufflings = _shufflingService.GetAllShufflings(true, true, 0, 100).Result;
+            }
+        }
+
+        private void TestGetAssignedShufflings()
+        {
+            using (Logger = new TestsessionLogger(_logger))
+            {
+                // Just testing for exceptions here
+                // ReSharper disable once UnusedVariable
+                var test = _shufflingService.GetAssignedShufflings(TestSettings.Account1).Result;
+            }
+        }
+
+        private void TestGetHoldingShufflings()
+        {
+            using (Logger = new TestsessionLogger(_logger))
+            {
+                var holdingId = 9836948456938609454;
+                var shufflings = _shufflingService.GetHoldingShufflings(holdingId, includeFinished: true).Result;
+
+                foreach (var shuffling in shufflings.Shufflings)
+                {
+                    AssertEquals(holdingId, shuffling.HoldingId, nameof(shuffling.HoldingId));
+                    AssertEquals(HoldingType.Asset, shuffling.HoldingType, nameof(shuffling.HoldingType));
+                    AssertIsNull(shuffling.HoldingInfo, nameof(shuffling.HoldingInfo));
+                }
+            }
+        }
+
+        private void TestGetShufflers()
+        {
+            using (Logger = new TestsessionLogger(_logger))
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void TestGetShuffling()
+        {
+            using (Logger = new TestsessionLogger(_logger))
+            {
+                var knownShuffling = _shufflingService.GetShuffling(_knownShufflingId).Result;
+                AssertKnownShuffling(knownShuffling);
+            }
+        }
+
+        private void TestGetShufflingParticipants()
+        {
+            using (Logger = new TestsessionLogger(_logger))
+            {
+                var participants = _shufflingService.GetShufflingParticipants(_knownShufflingId).Result;
+                var list = participants.Participants.ToList();
+                var me = list.Single(p => p.AccountRs == TestSettings.Account1.AccountRs);
+
+                AssertEquals(5, list.Count, nameof(participants.Participants));
+                list.ForEach(p => AssertEquals(ShufflingParticipantState.Verified, p.State, nameof(p.State)));
+                AssertEquals(TestSettings.Account1.AccountId, me.AccountId, nameof(me.AccountId));
+                AssertEquals("NXT-EVHD-5FLM-3NMQ-G46NR", me.NextAccountRs, nameof(me.NextAccountRs));
+                AssertEquals(16992224448242675179, me.NextAccountId, nameof(me.NextAccountId));
+                AssertEquals(_knownShufflingId, me.ShufflingId, nameof(me.ShufflingId));
+            }
         }
     }
 }
