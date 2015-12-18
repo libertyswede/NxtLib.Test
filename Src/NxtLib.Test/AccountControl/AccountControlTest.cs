940c1315e3b01ebc459bdd30d5c3394bb613c46c
Liberty Swede <libertyswede@outlook.com>
2015-12-18 17:00:23 +0100

    TestSetPhasingOnlyControl

--- Src/NxtLib.Test/AccountControl/AccountControlTest.cs
+++ Src/NxtLib.Test/AccountControl/AccountControlTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Extensions.Logging;
 using NxtLib.AccountControl;
 using NxtLib.VotingSystem;
@@ -29,9 +30,26 @@ namespace NxtLib.Test.AccountControl
         {
             using (Logger = new TestsessionLogger(_logger))
             {
-                var phasingOnlyControl = _accountControlService.SetPhasingOnlyControl(VotingModel.Account, 1,
-                    CreateTransaction.CreateTransactionByPublicKey(), 1000, VotingModel.Account, null,
-                    new[] {TestSettings.Account2.AccountRs}, Amount.CreateAmountFromNxt(1000)).Result;
+                var votingModel = VotingModel.Account;
+                var controlQuorum = 1;
+                var byPublicKey = CreateTransaction.CreateTransactionByPublicKey();
+                var controlMinBalance = 1000;
+                var controlMinBalanceModel = VotingModel.Nqt;
+                var controlMaxFees = Amount.CreateAmountFromNxt(1000);
+
+                var phasingOnlyControl = _accountControlService.SetPhasingOnlyControl(votingModel, controlQuorum,
+                    byPublicKey, controlMinBalance, controlMinBalanceModel, null,
+                    new[] {TestSettings.Account2.AccountRs}, controlMaxFees).Result;
+
+                var attachment = (AccountControlSetPhasingOnlyAttachment)phasingOnlyControl.Transaction.Attachment;
+
+                AssertEquals((int)votingModel, (int)attachment.PhasingVotingModel, nameof(attachment.PhasingVotingModel));
+                AssertEquals(controlQuorum, attachment.PhasingQuorum, nameof(attachment.PhasingQuorum));
+                AssertEquals(controlMinBalance, attachment.PhasingMinBalance, nameof(attachment.PhasingMinBalance));
+                AssertEquals((int)controlMinBalanceModel, (int)attachment.PhasingMinBalanceModel, nameof(attachment.PhasingMinBalanceModel));
+                AssertEquals(controlMaxFees.Nqt, attachment.ControlMaxFees.Nqt, nameof(attachment.ControlMaxFees));
+                AssertEquals(1, attachment.PhasingWhitelist.Count(), "attachment.PhasingWhitelist.Count()");
+                AssertEquals(attachment.PhasingWhitelist.Single(), TestSettings.Account2.AccountId, nameof(attachment.PhasingWhitelist));
             }
         }
     }
