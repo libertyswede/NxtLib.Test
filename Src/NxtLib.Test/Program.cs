4dd00dc077da6f2771e9ba868fd214542ee175a3
Liberty Swede <libertyswede@outlook.com>
2016-08-09 08:20:57 +0200

    Setup of app

--- Src/NxtLib.Test/Program.cs
+++ Src/NxtLib.Test/Program.cs
@@ -1,14 +1,55 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Logging;
+using System.Collections.ObjectModel;
 
 namespace NxtLib.Test
 {
     public class Program
     {
+        private static readonly Dictionary<string, string> InternalSettings = new Dictionary<string, string>();
+        public static readonly ReadOnlyDictionary<string, string> AppSettings = new ReadOnlyDictionary<string, string>(InternalSettings);
+        private static IServiceProvider _serviceProvider;
+        private static ServiceFactory _serviceFactory;
+
         public static void Main(string[] args)
         {
+            var applicationEnvironment = PlatformServices.Default.Application;
+            AddConfigurationSources(applicationEnvironment);
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddSingleton(provider => applicationEnvironment);
+            _serviceProvider = services.BuildServiceProvider();
+            ConfigureServices(services);
+            //_serviceProvider = services.BuildServiceProvider(); //Needed?
+        }
+
+        private static void AddConfigurationSources(ApplicationEnvironment applicationEnvironment)
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.SetBasePath(applicationEnvironment.ApplicationBasePath);
+            configBuilder.AddJsonFile("config.json");
+            configBuilder.AddJsonFile("config-Development.json", true);
+            var configRoot = configBuilder.Build();
+            foreach (var configurationSection in configRoot.GetChildren())
+            {
+                InternalSettings.Add(configurationSection.Key, configurationSection.Value);
+            }
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var loggerFactory = _serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddConsole(LogLevel.Debug);
+            _serviceFactory = new ServiceFactory(TestSettings.NxtServerUrl);
+
+            // TODO: Fix this
+            //services.AddTransient(provider => loggerFactory.CreateLogger(""));
+
+
         }
     }
 }
---
+++ Src/NxtLib.Test/TestSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NxtLib.Test
+{
+    public static class TestSettings
+    {
+        public static string NxtServerUrl => Program.AppSettings["NxtServerUrl"];
+        public static bool RunCostlyTests => Convert.ToBoolean(Program.AppSettings["RunCostlyTests"]);
+        public static AccountWithPublicKey Account1 { get; set; }
+        public static AccountWithPublicKey Account2 { get; set; }
+        public static string SecretPhrase1 => Program.AppSettings["SecretPhrase1"];
+        public static string SecretPhrase2 => Program.AppSettings["SecretPhrase2"];
+        public static ulong ExistingAssetId => Convert.ToUInt64(Program.AppSettings["ExistingAssetId"]);
+        public static ulong ExistingCurrencyId => Convert.ToUInt64(Program.AppSettings["ExistingCurrencyId"]);
+        public static int MaxHeight { get; set; }
+        public static ulong PollId { get; set; }
+        public static ulong TaggedDataTransactionId => Convert.ToUInt64(Program.AppSettings["TaggedDataTransactionId"]);
+        public static ulong SentTextMessageTransactionId => Convert.ToUInt64(Program.AppSettings["SentTextMessageTransactionId"]);
+        public static ulong SentDataMessageTransactionId => Convert.ToUInt64(Program.AppSettings["SentDataMessageTransactionId"]);
+    }
+}
